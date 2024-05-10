3c026a3bf7b76da8294d70ff868e9192dcd49047
Yahav Biran <yahavb@me.com>
2024-05-09 22:03:46 -0700

    export height and width

--- sd_hf_serve/app/run-sd-optimum-neuron.py
+++ sd_hf_serve/app/run-sd-optimum-neuron.py
@@ -11,6 +11,8 @@ pod_name=os.environ['POD_NAME']
 device=os.environ["DEVICE"]
 compiled_model_id=os.environ['COMPILED_MODEL_ID']
 num_inference_steps=int(os.environ['NUM_OF_RUNS_INF'])
+height=int(os.environ['HEIGHT'])
+width=int(os.environ['WIDTH'])
 
 from optimum.neuron import NeuronStableDiffusionPipeline
 pipe = NeuronStableDiffusionPipeline.from_pretrained(compiled_model_id)
@@ -28,12 +30,12 @@ image = pipe(**model_args).images[0]
 
 app = FastAPI()
 io = gr.Interface(fn=text2img,inputs=["text"],
-    outputs = [gr.Image(height=832, width=448), "text"],
-    title = 'Stable Diffusion 2.1 in AWS EC2 ' + device + ' instance; pod name ' + pod_name)
+    outputs = [gr.Image(height=height, width=width), "text"],
+    title = compiled_model_id + ' in AWS EC2 ' + device + ' instance; pod name ' + pod_name)
 
 @app.get("/")
 def read_main():
-  return {"message": "This is Stable Diffusion 2.1 pod " + pod_name + " in AWS EC2 " + device + " instance; try /serve"}
+  return {"message": "This is " + compiled_model_id + " pod " + pod_name + " in AWS EC2 " + device + " instance; try /serve"}
 
 @app.get("/health")
 def healthy():
